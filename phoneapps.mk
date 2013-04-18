8dd2f29ff47fcaff68beea97e8b01de9b89915d1
juanlianxiaojiang <1152239468@qq.com>
2013-04-18 21:40:45 +0800

    first commit

---
+++ phoneapps.mk
@@ -0,0 +1,23 @@
+local-phone-apps = $(private-phone-apps)
+
+private-phone-apps := ApplicationsProvider \
+					  BackupRestoreConfirmation \
+					  CertInstaller \
+					  DefaultContainerService \
+					  DrmProvider \
+					  KeyChain \
+					  LiveWallpapers \
+                                          LiveWallpapersPicker \
+					  MagicSmokeWallpapers \
+                                          MediaProvider \
+					  NoiseField \
+					  PhaseBeam \
+					  SharedStorageBackup \
+					  UserDictionaryProvider \
+					  VisualizationWallpapers \
+					  WAPPushManager
+
+private-lockscreen-apps := 
+
+private-phone-apps2 :=  
+#for ace
